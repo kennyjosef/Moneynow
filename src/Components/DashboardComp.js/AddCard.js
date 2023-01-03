21e53345dc9a6beddb056f9f04e80834c3260f75
kennyjosef <idowukehinde28@gmail.com>
2023-01-03 14:07:42 +0100

    Implementing the modals

--- src/Components/DashboardComp.js/AddCard.js
+++ src/Components/DashboardComp.js/AddCard.js
@@ -16,9 +16,31 @@ function AddCard() {
 
       <Modal show={show} onHide={handleClose} className="down">
         <Modal.Header closeButton className='color'>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Add Bank</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Body>
+            <p>Add Your One Time Bank Transfer Account</p>
+            <div>
+              <label htmlFor="bank">Bank Details</label>
+              <select name="banks" id="banks">
+                <option value="access">Access Bank plc</option>
+                <option value="first">Frst Bank plc</option>
+                <option value="polaris">Polaris Bank plc</option>
+                <option value="wema">Wema Bank plc</option>
+                <option value="gtb">GTBank plc</option>
+                <option value="sterlin">Sterlin Bank plc</option>
+                <option value="lafenda">Lafenda Mircrofinance Bank plc</option>
+                <option value="mortage">Mortage Bank plc</option>
+                <option value="union">Union Bank </option>
+                <option value="fcmb">FCMB</option>
+                <option value="palm">Palm pay</option>
+              </select>
+            </div>
+            <div>
+              <label htmlFor="accountName">Account Name</label>
+              <input type="text" name="accountName" id="accountName" placeholder='David Ade' />
+            </div>
+        </Modal.Body>
         <Modal.Footer>
           <button  onClick={handleClose}>
             Add Bank
