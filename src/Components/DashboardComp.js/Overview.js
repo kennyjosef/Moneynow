ece267666bdc484443672b872e65989552f4de10
kennyjosef <idowukehinde28@gmail.com>
2023-01-13 18:29:01 +0100

    working on transaction history

--- src/Components/DashboardComp.js/Overview.js
+++ src/Components/DashboardComp.js/Overview.js
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from "react";
 import "./Overview.css";
 import eye from "../../Assets/eyeopened.svg";
 import axios from "axios";
+import _ from 'lodash';
 import Transaction from "./Transaction";
-
+import ScaleLoader from 'react-spinners/ScaleLoader'
 
 const Overview = () => {
   const [show, setShow]=useState(true)
@@ -11,41 +12,56 @@ const Overview = () => {
   const [transactionEvent, setTransactionEvent]= useState([])
   const [transactionMade, setTransactionMade] = useState(0)
   const [loading, setLoading]= useState(false)
+  const [paginated,setPaginated]= useState()
+  const pageSize= 10
   const [currentPage, setCurrentPage] = useState(1)
-  const [postPerPage, setPostPerPage] = useState(10)
+  let [color, setColor] = useState(" rgb(238, 88, 238)");
+  const override  = {
+    display: "block",
+    borderColor: "rgb(238, 88, 238)",
+  };
   const url="https://moneynow.onrender.com/api/userbalance"
   const transaction ="https://moneynow.onrender.com/transaction/usertransaction"
   const token= localStorage.getItem("token")
   const getUserBalance=()=>{
     axios.get(url, 
       {headers:{authorization:`Bearer ${token}`}})
-    .then(res=>{
-      setBalance(res.data.balance)
-    })
-    .catch(error=>{
-      console.log(error)
-    })
-  }
-  getUserBalance();
-  useEffect(()=>{
-  const getTransaction= ()=>{
-    setLoading(true)
-    axios.get(transaction,
-      {headers:{authorization:`Bearer ${token}`}})
       .then(res=>{
-        console.log("Transacttion is", res)
-        setTransactionEvent(res.data.userTransactions)
-        setTransactionMade(res.data.allTransactions)
-        setLoading(false)
+        setBalance(res.data.balance)
       })
       .catch(error=>{
         console.log(error)
       })
-  }
-    getTransaction()
-  },[])
+    }
+    getUserBalance();
+    useEffect(()=>{
+      const getTransaction= ()=>{
+        setLoading(true)
+        axios.get(transaction,
+          {headers:{authorization:`Bearer ${token}`}})
+          .then(res=>{
+            console.log("Transacttion is", res)
+            setTransactionEvent(res.data.userTransactions)
+            setTransactionMade(res.data.allTransactions)
+            setPaginated(_ (res.data.userTransactions).slice(0).take(pageSize).value())
+            setLoading(false)
+          })
+          .catch(error=>{
+            console.log(error)
+          })
+        }
+        getTransaction()
+      },[])
+      console.log(localStorage.getItem("time"))
+      const pageCount= transactionEvent?Math.ceil(transactionEvent.length/pageSize):0;
+      const pages= _.range(1, pageCount+1)
+      const pagination=(pageNo)=>{
+        setCurrentPage(pageNo)
+        const startIndex=(pageNo-1)*pageSize
+        const paginated = _ (transactionEvent).slice(startIndex).take(pageSize).value();
+        setPaginated(paginated)
+      }
   console.log("transaction event", transactionEvent)
-  // console.log(transactionMade)
   return (
     <div className="overview back">
       <div className="balancegroup">
@@ -62,28 +78,49 @@ const Overview = () => {
         </div>
       </div>
       <div className="tableContainer">
-      <p>Transaction History <span>{transactionMade}</span></p>
-      <div className="father">
-          <p>Transaction Type</p>
-          <p>Amount</p>
-          <p>Date & Time</p>
-          <p>Status</p>
+      <p className="transme">Transaction History <span>{transactionMade}</span></p>
+           {
+            loading ?
+            <ScaleLoader color={color} loading={loading}size={50} cssOverride={override} aria-label="Loading Spinner" data-testid="loader"/>
+            :
+            <>
+           <table className="table">
+              <thead>
+                <tr>
+                  <th>Transaction Type</th>
+                  <th>Amount</th>
+                  <th>Date & Time</th>
+                  <th>Status</th>
+                </tr>
+                </thead>
+                <tbody>
+                  {
+                    paginated?.map(items=> <tr key={items.id}>
+                      <td>{items.purpose}</td>
+                      <td>{items.amount.$numberDecimal}</td>
+                      <td>{items.createdAt}</td>
+                      <td>{items.transactionStatus}</td>
+                    </tr>)
+                  }
+
+                </tbody>
+           </table>
+          <nav className="d-flex justify-content-center">
+            <ul className="pagination">
+              {
+                pages.map((page)=>(
+                  
+                  <li className={page=== currentPage? "page-item active": "page-item"}>
+                    <p className="page-link" onClick={()=>pagination(page)}>{page}</p>
+                  </li>
+                ))
+              }
+            </ul>
+          </nav>
+            </>
+           }
+            
       </div>
-        <div>
-         
-          {
-            transactionEvent.map(items=>(
-              <Transaction  amount={items.amount.$numberDecimal} 
-              status={items.transactionStatus} 
-              date={items.createdAt}
-              type={items.purpose}
-              key={items.id}/>
-            )
-            )
-          }
-        </div>
-      </div>
-      {/* <button onClick={getTransaction}>get transaction</button> */}
     </div>
   );
 };
--- src/Components/DashboardComp.js/Transaction.js
+++ src/Components/DashboardComp.js/Transaction.js
@@ -1,14 +1,7 @@
 import React from 'react'
 import classes from './Transaction.module.css'
 const Transaction = ({amount, status, type, date}) => {
-   const style={
-    width: "100%",
-    borderCollapse : "collapse"
-   }
-   const tr={
-    width:'100%',
-    border:"1px solid blue"
-   }
+ 
   return (
     <div className={classes.father}>
           <p>{type}</p>
